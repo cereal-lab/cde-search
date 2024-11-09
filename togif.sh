23c11ecdb3ed014efe54b0839760dc1376f0bea5
dvitel <viteldmitry@gmail.com>
2024-11-09 10:37:12 -0500

    restructuring repo

--- togif.sh
+++ togif.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #convert -delay 10 -loop 0 img/*[13579].png res1.gif
 #convert -delay 10 -loop 0 img/*[02468].png res2.gif
-convert -delay 20 -loop 1 img/*.png ${1:-res}.gif
+convert -delay 20 -loop 1 img/*.png gifs/${1:-res}.gif
