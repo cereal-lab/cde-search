6b7a24acaef39c80039ba40a9a7fd616ce144251
dvitel <viteldmitry@gmail.com>
2025-01-24 13:56:31 -0500

    fix bug with iterator front cov

--- front_coverage.py
+++ front_coverage.py
@@ -57,8 +57,8 @@ def full_coverage_selection(population, fitnesses, coverage_selection_size = 2, 
         indexes = subgroup_start + choices
         full_coverage.selected = iter(indexes)
         next_index = next(full_coverage.selected)
-        if len(population) >= 6 and str(population[5]) == "x6":
-            print(f"selected: ss {subgroup_start}, ssize {subgroup_size} choice {choices} indexes {indexes} next {next_index}")
+        # if len(population) >= 6 and str(population[5]) == "x6":
+        #     print(f"selected: ss {subgroup_start}, ssize {subgroup_size} choice {choices} indexes {indexes} next {next_index}")
     # best = population[next_index]
     return next_index
 
@@ -152,6 +152,7 @@ def select_full_test_coverage(front_selection_size, interactions, test_program_s
     if len(groups) == 0:
         groups.append(len(selected))
     indexes = list(zip([0, *np.cumsum(groups).tolist()], groups))
+    full_coverage.selected = iter([])
     full_coverage.subgroups = cycle(indexes)
     return np.array(selected)
 
@@ -210,155 +211,3 @@ if __name__ == '__main__':
     print(best_prog)
     print(stats)
     pass    
-
-# def select_program_totally_best(ints: np.ndarray, allowed_program_mask: np.ndarray, allowed_test_mask: np.ndarray, num_hardest_tests = 2):
-#     # selected_test_id = test_selection(ints, allowed_test_mask)
-#     allowed_test_indexes = np.where(allowed_test_mask)[0]
-#     test_stats = np.sum(ints[:, allowed_test_mask], axis = 0) #sum accross columns to see how many programs pass each test    
-#     test_stats[test_stats == 0] = np.inf
-
-#     allowed_program_indexes = np.where(allowed_program_mask)[0]
-#     whole_program_weights = ints[allowed_program_indexes][:, allowed_test_indexes] * test_stats
-#     whole_program_weights[whole_program_weights == 0] = np.inf
-#     whole_program_weights_sorted = np.sort(whole_program_weights, axis = 1)
-#     whole_program_weights_subset = whole_program_weights_sorted[:, :num_hardest_tests]
-#     program_indexes = np.lexsort(whole_program_weights_subset.T)
-
-#     # program_win_counts = np.sum(whole_program_weights != 0, axis = 1)
-#     # program_weights = np.sum(whole_program_weights, axis = 1) 
-#     # program_weights[program_win_counts == 0] = np.inf
-#     # program_weights[program_win_counts != 0] /= program_win_counts[program_win_counts != 0]
-#     # best_program_min_w = np.min(program_weights)
-#     # best_program_id_ids = np.where(program_weights == best_program_min_w)[0]
-#     # best_program_ids = allowed_program_indexes[best_program_id_ids] #the program that passes the most tests including selected rare test
-#     best_program_id_id = program_indexes[0] #default_rnd.choice(best_program_ids, 1)[0]
-#     best_program_id = allowed_program_indexes[best_program_id_id]
-#     allowed_program_mask[best_program_id] = False
-#     solved_test_ids_by_best = np.where(ints[best_program_id] != 0)[0]
-#     allowed_test_mask[solved_test_ids_by_best] = False
-#     best_test_stats = np.copy(whole_program_weights[best_program_id])
-#     best_test_stats[best_test_stats == 0] = np.inf
-#     selected_test_id_id = np.argmin(best_test_stats)
-#     selected_test_id = allowed_test_indexes[selected_test_id_id]
-#     return best_program_id, selected_test_id
-
-
-# def select_program_shallow(ints: np.ndarray, selected_test_id: int, population: list, allowed_program_mask: np.ndarray, allowed_test_mask: np.ndarray):
-#     allowed_program_indexes = np.where(allowed_program_mask)[0]
-#     program_candidate_id_ids = np.where(ints[allowed_program_indexes, selected_test_id] != 0)[0]
-#     program_candidate_ids = allowed_program_indexes[program_candidate_id_ids]
-#     program_depths = np.array([population[i].get_depth() for i in program_candidate_ids])
-#     best_program_id = program_candidate_ids[np.argmin(program_depths)]
-#     allowed_program_mask[best_program_id] = False
-#     solved_test_ids_by_best = np.where(ints[best_program_id] != 0)[0]
-#     allowed_test_mask[solved_test_ids_by_best] = False
-#     return best_program_id    
-
-
-# full_test_coverage_best_program = partial(select_full_test_coverage, select_program_totally_best)
-
-# def select_rand_coverage(selection_size, interactions): 
-#     ''' Randomly samples programs from interactions '''
-#     program_ids = default_rnd.choice(interactions.shape[0], selection_size, replace = False)
-#     return program_ids
-
-# def sorting_test_pairs_easy_first(test_pairs, test_stats):
-#     sorted_pairs = sorted(test_pairs, key = lambda x: test_stats[x[0]] + test_stats[x[1]], reverse=True)
-#     return sorted_pairs
-
-# def sorting_test_pairs_hard_first(test_pairs, test_stats):
-#     test_stats[test_stats == 0] = np.inf
-#     sorted_pairs = sorted(test_pairs, key = lambda x: test_stats[x[0]] + test_stats[x[1]])
-#     return sorted_pairs
-
-# def sorting_test_pairs_random(test_pairs, test_stats):
-#     return default_rnd.permutation(test_pairs).tolist()
-
-# pair_coverage_subgroups = iter([]) #pos and lengthes of each subgroup in population, should be cycling iterator
-# pair_coverage_selected = iter([])
-
-# def subgroup_selection(population):
-#     global pair_coverage_subgroups
-#     global pair_coverage_selected
-#     if (next_index := next(pair_coverage_selected, None)) is None:
-#         pos, (l1, l2) = next(pair_coverage_subgroups)
-#         best_index1 = pos + default_rnd.choice(l1)
-#         best_index2 = pos + l1 + default_rnd.choice(l2)
-#         pair_coverage_selected = iter([best_index1, best_index2])
-#         next_index = next(pair_coverage_selected)
-#     best = population[next_index]
-#     return best
-    
-# prev_test_pairs = []
-# def select_pair_coverage(pair_sorting, selection_size, interactions, population, persist_test_pair = False):
-#     ''' Picks two tests to be solved in parent programs combination 
-#         There could be many ways to pick a pair, we have some hypothesis but need to check different pair selections
-#         Hypothesis 1: tests that are easily solvable is easy to recombine
-#         Hypothesis 2: solving first pairs of most difficult tests gives better convergence speed 
-#         Hypothesis 3: random pair selection is good enough.
-#         NOTE: NOTE: We need to measure average time to recombine targeted test pair         
-#         - we need metric to estimate the effect
-#     '''
-#     global prev_test_pairs
-#     global pair_coverage_subgroups
-#     ints = interactions
-#     selected = []
-#     test_pairs = combinations(range(ints.shape[1]), 2)
-#     filtered_test_pairs = [] # tests that are not currently solved together
-#     prev_test_pairs_set = set(prev_test_pairs)
-#     if persist_test_pair:
-#         for test1, test2 in test_pairs:
-#             # if np.all(np.logical_not(np.logical_and(ints[:, test1], ints[:, test2]))) and not((test1, test2) in prev_test_pairs_set):
-#             if not((test1, test2) in prev_test_pairs_set):
-#                 filtered_test_pairs.append((test1, test2))
-#     else:
-#         filtered_test_pairs = list(test_pairs)
-#     test_stats = np.sum(ints, axis = 0)
-#     # if persist_test_pair:
-#     #     pair_sorting = partial(holder.pair_sorting, pair_sorting)
-#     sorted_test_pairs = pair_sorting(filtered_test_pairs, test_stats)
-#     sorted_test_pairs = prev_test_pairs + sorted_test_pairs
-#     sorted_test_pairs_iter = iter(sorted_test_pairs)
-#     group_lengths = []
-#     new_test_pairs = []
-#     while len(selected) < selection_size:
-#         test_id1, test_id2 = next(sorted_test_pairs_iter, (None, None)) #we target first this pair of tests
-#         if test_id1 is None:
-#             break
-#         new_test_pairs.append((test_id1, test_id2))
-#         candidate_program_ids1 = np.where(ints[:, test_id1] != 0)[0]
-#         candidate_program_ids2 = np.where(ints[:, test_id2] != 0)[0]
-#         # fixed_candidate_program_ids1 = np.setdiff1d(candidate_program_ids1, candidate_program_ids2, assume_unique=True)
-#         # fixed_candidate_program_ids2 = np.setdiff1d(candidate_program_ids2, candidate_program_ids1, assume_unique=True)
-#         # candidate_program_ids1 = fixed_candidate_program_ids1
-#         # candidate_program_ids2 = fixed_candidate_program_ids2
-#         if len(candidate_program_ids1) > len(candidate_program_ids2):
-#             candidate_program_ids1, candidate_program_ids2 = candidate_program_ids2, candidate_program_ids1
-#         if len(candidate_program_ids1) == 0:
-#             continue
-#         half_avail_size = (selection_size - len(selected)) // 2
-#         if len(candidate_program_ids1) > half_avail_size:
-#             candidate_program_ids1 = default_rnd.choice(candidate_program_ids1, half_avail_size, replace=False)
-#         other_avail_size = selection_size - len(selected) - len(candidate_program_ids1)
-#         if len(candidate_program_ids2) > other_avail_size:
-#             candidate_program_ids2 = default_rnd.choice(candidate_program_ids2, other_avail_size, replace=False)
-#         group_lengths.append((len(candidate_program_ids1), len(candidate_program_ids2)))
-#         selected.extend(candidate_program_ids1)
-#         selected.extend(candidate_program_ids2)
-#     if persist_test_pair:
-#         prev_test_pairs = new_test_pairs
-#     indexes = list(zip([0, *np.cumsum([x + y for x, y in group_lengths]).tolist()], group_lengths))
-#     pair_coverage_subgroups = cycle(indexes)
-#     return np.array(selected)
-
-# pair_coverage_easy_first = partial(select_pair_coverage, sorting_test_pairs_easy_first)
-
-# pair_coverage_hard_first = partial(select_pair_coverage, sorting_test_pairs_hard_first)
-
-# pair_coverage_random = partial(select_pair_coverage, sorting_test_pairs_random)
-
-# pair_coverage_easy_first_persist = partial(select_pair_coverage, sorting_test_pairs_easy_first, persist_test_pair = True)
-
-# pair_coverage_hard_first_persist = partial(select_pair_coverage, sorting_test_pairs_hard_first, persist_test_pair = True)
-
-# pair_coverage_random_persist = partial(select_pair_coverage, sorting_test_pairs_random, persist_test_pair = True)
